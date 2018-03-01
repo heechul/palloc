9d7bac5f4c28a46a3f3be4911d187dee84f08f5c
Heechul Yun <heechul.yun@ku.edu>
2018-03-01 01:31:32 +0000

    works on odroid-xu4

--- detect-mc-mapping-pagemap.sh
+++ detect-mc-mapping-pagemap.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 killall -9 mc-mapping-pagemap
 echo "Run a background task on core1-3"
-./mc-mapping-pagemap -c 1 -p 0.1 -i 100000000000 -b 0 >& /dev/null &
-./mc-mapping-pagemap -c 2 -p 0.1 -i 100000000000 -b 0 >& /dev/null &
+
+for c in 1 2; do
+	./mc-mapping-pagemap -c $c -w 16 -p 0.2 -i 100000000000 -b 0 >& /dev/null &
+done
 sleep 1
 
 echo "Now run the test"
